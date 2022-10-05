923e14977bed2dd222fcd39c4f2e4f6653dd0393
mao <1296193245@qq.com>
2022-10-05 20:20:09 +0800

    android_Notification

--- app/src/main/java/mao/android_notification/MainActivity.java
+++ app/src/main/java/mao/android_notification/MainActivity.java
@@ -13,6 +13,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -23,29 +26,46 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+//        {
+//            NotificationChannel notificationChannel = new NotificationChannel("1", "测试通知", NotificationManager.IMPORTANCE_HIGH);
+//            notificationManager.createNotificationChannel(notificationChannel);
+//        }
+//
+//        PendingIntent pendingIntent = PendingIntent.
+//                getActivity(this, 0, new Intent(MainActivity.this, MainActivity2.class), 0);
+//
+//        Notification notification = new NotificationCompat.Builder(this, "1")
+//                .setContentTitle("标题")
+//                .setContentText("内容")
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                .setColor(Color.rgb(255, 0, 0))
+//                .setAutoCancel(true)
+//                .setContentIntent(pendingIntent)
+//                .build();
+//
+//        notificationManager.notify(1, notification);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        createNotificationChannel("2", "测试通知");
+
+        findViewById(R.id.Button1).setOnClickListener(new View.OnClickListener()
         {
-            NotificationChannel notificationChannel = new NotificationChannel("1", "测试通知", NotificationManager.IMPORTANCE_HIGH);
-            notificationManager.createNotificationChannel(notificationChannel);
-        }
-
-        PendingIntent pendingIntent = PendingIntent.
-                getActivity(this, 0, new Intent(MainActivity.this, MainActivity2.class), 0);
-
-        Notification notification = new NotificationCompat.Builder(this, "1")
-                .setContentTitle("标题")
-                .setContentText("内容")
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setColor(Color.rgb(255, 0, 0))
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent)
-                .build();
-
-        notificationManager.notify(1, notification);
-
-
+            @Override
+            public void onClick(View v)
+            {
+                sendBaseNotification("2", new Random().nextInt(100000), "标题", "内容");
+            }
+        });
+        findViewById(R.id.Button2).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                sendBaseNotification("2", new Random().nextInt(100000), "标题", "内容", MainActivity2.class);
+            }
+        });
     }
 
 
@@ -58,7 +78,6 @@ public class MainActivity extends AppCompatActivity
     private void createNotificationChannel(String id, String name)
     {
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
         {
             NotificationChannel notificationChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_HIGH);
@@ -78,15 +97,12 @@ public class MainActivity extends AppCompatActivity
     private void sendBaseNotification(String channelId, int id, String title, String content)
     {
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-
         Notification notification = new NotificationCompat.Builder(this, channelId)
                 .setContentTitle(title)
                 .setContentText(content)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setAutoCancel(true)
                 .build();
-
         notificationManager.notify(id, notification);
     }
 
@@ -99,14 +115,12 @@ public class MainActivity extends AppCompatActivity
      * @param content   内容
      * @param cls       点击后要跳转到的Activity类
      */
-    private void sendBaseNotification(String channelId, int id, String title, String content, Class<AppCompatActivity> cls)
+    private void sendBaseNotification(String channelId, int id, String title, String content, Class<?> cls)
     {
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
         PendingIntent pendingIntent = PendingIntent.
                 getActivity(this, 0,
                         new Intent(this, cls), 0);
-
         Notification notification = new NotificationCompat.Builder(this, channelId)
                 .setContentTitle(title)
                 .setContentText(content)
@@ -114,7 +128,6 @@ public class MainActivity extends AppCompatActivity
                 .setAutoCancel(true)
                 .setContentIntent(pendingIntent)
                 .build();
-
         notificationManager.notify(id, notification);
     }
 
